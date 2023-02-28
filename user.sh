f0a82c714cc591e973f27b002bec77acb3cb4a57
SK1PP3R <56693392+SK1PP3R@users.noreply.github.com>
2023-02-28 12:28:34 +0100

    Update user.sh

--- user.sh
+++ user.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 if [ ! "$(id -u steam)" -eq "$UID" ]; then 
-        echo "Changing steam uid to $UID."
         usermod -o -u "$UID" steam ; 
 fi
 if [ ! "$(id -g steam)" -eq "$GID" ]; then 
-        echo "Changing steam gid to $GID."
         groupmod -o -g "$GID" steam ; 
 fi
 chown -R steam:steam /ark /home/steam
