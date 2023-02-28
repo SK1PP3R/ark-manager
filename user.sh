e26585f4c42af27816e41c10afc5e40b4ad80624
SK1PP3R <56693392+SK1PP3R@users.noreply.github.com>
2023-02-28 14:03:50 +0100

    Update user.sh

--- user.sh
+++ user.sh
@@ -35,7 +35,9 @@ echo "Start Server...";
 #su - steam -c "cd /ark/server/ShooterGame/Binaries/Linux && ./ShooterGameServer "${SERVERMAP}?listen?SessionName=${SESSIONNAME}?Port=${STEAMPORT}?bRawSockets=${STEAMPORT}?QueryPort=${PORT}?usGamePort=${PORT}?ServerAdminPassword=${ADMINPASSWORD}?GameModIds=${GAME_MOD_IDS}?MaxPlayers=${MAX_PLAYERS}?RCONEnabled=${RCON_ENABLED}?RCONPort=${RCON_PORT}?serverPVE=${DISABLE_PVP}" -server -log $(if [ ${DISABLE_BATTLEYE} -eq 1 ]; then echo "-NoBattlEye"; fi)"
 start="cd /ark/server/ShooterGame/Binaries/Linux && ./ShooterGameServer "${SERVERMAP}?listen?SessionName=${SESSIONNAME}?Port=${STEAMPORT}?bRawSockets=${STEAMPORT}?QueryPort=${PORT}?usGamePort=${PORT}?ServerAdminPassword=${ADMINPASSWORD}?GameModIds=${GAME_MOD_IDS}?MaxPlayers=${MAX_PLAYERS}?RCONEnabled=${RCON_ENABLED}?RCONPort=${RCON_PORT}?serverPVE=${DISABLE_PVP}" -server -log"
 if [ "${DISABLE_BATTLEYE}" = "1" ]; then
+ echo "Start ARK without BattlEye."
  su - steam -c "$start -NoBattlEye"
 else
+ echo "Start ARK with BattlEye."
  su - steam -c "$start"
 fi
