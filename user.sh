2c2dcb779461197b85a8498eb4dc7d7b1bf20237
SK1PP3R <56693392+SK1PP3R@users.noreply.github.com>
2023-02-28 13:28:03 +0100

    Update user.sh

--- user.sh
+++ user.sh
@@ -29,7 +29,5 @@ if [ ! -f "/ark/GameUserSettings.ini" ]; then
 fi
 
 echo -n "Server wird eingerichtet... "
-#su -p - steam -c 'cd /home/steam && ./steamcmd.sh +login anonymous +force_install_dir /ark/server +app_update 376030 validate +quit'
-#cd /ark/server/ShooterGame/Binaries/Linux && ./ShooterGameServer '${SERVERMAP}?listen?SessionName=${SESSIONNAME}?Port=${STEAMPORT}?bRawSockets=${STEAMPORT}?QueryPort=${PORT}?usGamePort=${PORT}?ServerAdminPassword=${ADMINPASSWORD}?GameModIds=${GAME_MOD_IDS}?MaxPlayers=${MAX_PLAYERS}?RCONEnabled=${RCON_ENABLED}?RCONPort=${RCON_PORT}?serverPVE=${DISABLE_PVP}' -server -log $(if [ $DISABLE_BATTLEYE -eq 1 ]; then echo '-NoBattlEye'; fi)
-#su - steam -c 'cd /ark/server/ShooterGame/Binaries/Linux && ./ShooterGameServer "${SERVERMAP}?listen?SessionName=${SESSIONNAME}?Port=${STEAMPORT}?bRawSockets=${STEAMPORT}?QueryPort=${PORT}?usGamePort=${PORT}?ServerAdminPassword=${ADMINPASSWORD}?GameModIds=${GAME_MOD_IDS}?MaxPlayers=${MAX_PLAYERS}?RCONEnabled=${RCON_ENABLED}?RCONPort=${RCON_PORT}?serverPVE=${DISABLE_PVP}" -server -log $(if [ ${DISABLE_BATTLEYE} -eq 1 ]; then echo "-NoBattlEye"; fi)'
-sleep 60000
+su -p - steam -c 'cd /home/steam && ./steamcmd.sh +login anonymous +force_install_dir /ark/server +app_update 376030 validate +quit'
+su - steam -c "cd /ark/server/ShooterGame/Binaries/Linux && ./ShooterGameServer "${SERVERMAP}?listen?SessionName=${SESSIONNAME}?Port=${STEAMPORT}?bRawSockets=${STEAMPORT}?QueryPort=${PORT}?usGamePort=${PORT}?ServerAdminPassword=${ADMINPASSWORD}?GameModIds=${GAME_MOD_IDS}?MaxPlayers=${MAX_PLAYERS}?RCONEnabled=${RCON_ENABLED}?RCONPort=${RCON_PORT}?serverPVE=${DISABLE_PVP}" -server -log $(if [ ${DISABLE_BATTLEYE} -eq 1 ]; then echo "-NoBattlEye"; fi)"
