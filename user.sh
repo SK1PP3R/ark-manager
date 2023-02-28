b4fd4e5a0e4ab5425a7650f70f7ceac7f1d1a081
SK1PP3R <56693392+SK1PP3R@users.noreply.github.com>
2023-02-28 12:43:42 +0100

    Update user.sh

--- user.sh
+++ user.sh
@@ -29,5 +29,5 @@ if [ ! -f "/ark/GameUserSettings.ini" ]; then
 fi
 
 echo -n "Server wird eingerichtet... "
-su -p - steam -c 'cd /home/steam && ./steamcmd.sh +login anonymous +force_install_dir /ark/server +app_update 376030 validate +quit' > /dev/null 2>&1
+su -p - steam -c 'cd /home/steam && ./steamcmd.sh +login anonymous +force_install_dir /ark/server +app_update 376030 validate +quit'
 cd /ark/ShooterGame/Binaries/Linux && ./ShooterGameServer '${SERVERMAP}?listen?SessionName=${SESSIONNAME}?Port=${STEAMPORT}?bRawSockets=${STEAMPORT}?QueryPort=${PORT}?usGamePort=${PORT}?ServerAdminPassword=${ADMINPASSWORD}?GameModIds=${GAME_MOD_IDS}?MaxPlayers=${MAX_PLAYERS}?RCONEnabled=${RCON_ENABLED}?RCONPort=${RCON_PORT}?serverPVE=${DISABLE_PVP}' -server -log $(if [ $DISABLE_BATTLEYE -eq 1 ]; then echo '-NoBattlEye'; fi)
